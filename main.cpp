cc101bf9e218fbfdfe72878b1376e91d89211045
Dinh Quan <quan@quan-ai-pro.domain.name>
2021-12-11 00:18:03 +0700

    reup model lần n

--- main.cpp
+++ main.cpp
@@ -3,14 +3,16 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
-#include <math.h>
+#include <cmath>
+#include <cstring>
 using namespace std;
+stringstream name;
 ILOSTLBEGIN;
 
 int main()
 {
-    freopen("/home/quan/Desktop/Ai_Do?/ORProject/TestDataFile/TSP/ulysses16.tsp", "rt", stdin);
-    freopen("solution_for_ulysses16MTZ.txt", "wt", stdout);
+    freopen("/home/quan/Desktop/Ai_Do?/ORProject/DataFile/TSP/ulysses22.tsp", "rt", stdin);
+    freopen("solution_for_ulysses22MTZ.txt", "wt", stdout);
     ios::sync_with_stdio(false);
     cin.tie(0), cout.tie(0);
 
@@ -20,8 +22,8 @@ int main()
     vector<double> xPos(n), yPos(n);
     for(int i=0; i<n; i++) {
         int index;
-        cin>>index;
-        cin>>xPos[i]>>yPos[i];
+        cin>> index;
+        cin>> xPos[i]>> yPos[i];
         xPos[i] *= 100;
         yPos[i] *= 100;
     }
@@ -44,25 +46,31 @@ int main()
         for(int i=0; i<n; i++) {
             x[i] = IloNumVarArray(env, n);
             for(int j=0; j<n; j++) {
-                x[i][j] = IloNumVar(env, 0, 1, ILOBOOL);
+                name<< "x_"<< i<< "."<< j;
+                x[i][j] = IloNumVar(env, 0, 1, ILOBOOL, name.str().c_str());
+                name.str("");
             }
         }
 
 
         /*----OBJECTIVE FUNCTION-------------------------*/
-        IloExpr obj(env);
+        IloExpr sumObj(env);
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
                 if(i!=j) {
-                    obj += cost[i][j]*x[i][j];
+                    sumObj += cost[i][j]*x[i][j];
                 }
             }
         }
-        model.add(IloMinimize(env , obj));
-        obj.end();
+        model.add(IloMinimize(env , sumObj));
+        sumObj.end();
 
 
         /*-----CONSTRAINTS----------------------------------*/
+        IloRangeArray inbound_cons(env, n);
+        IloRangeArray outbound_cons(env, n);
+        IloArray<IloRangeArray> MTZ(env, n);
+
         // for each i, the sum of all x[i][j] (i!=j) must equal 1
         IloExpr expr(env);
         for(int i=0; i<n; i++) {
@@ -71,9 +79,13 @@ int main()
                     expr += x[i][j];
                 }
             }
-            model.add(expr == 1);
+            name<< "inbound_"<< i;
+            inbound_cons[i] = IloRange(env, 1, expr, 1, name.str().c_str());
+            name.str("");
             expr.clear();
         }
+        model.add(inbound_cons);
+
         // for each j, the sum of all x[i][j] (i!=j) must equal 1
         for(int j=0; j<n; j++) {
             for(int i=0; i<n; i++) {
@@ -81,9 +93,13 @@ int main()
                     expr += x[i][j];
                 }
             }
-            model.add(expr == 1);
+            name<< "outbound_"<< j;
+            outbound_cons[j] = IloRange(env, 1, expr, 1, name.str().c_str());
+            name.str("");
             expr.clear();
         }
+        model.add(outbound_cons);
+
         /* u[i] is a dummy variable, if the solution has no subtour, the model can find the variables
         u[i] satisfy the constraint: u[i]-u[j]+n*x[i][j] <= n-1 with all i, j >=1 (i!=j). For easy of
         visualization, u[i] now represents the order of visits of the nodes. If
@@ -91,17 +107,24 @@ int main()
 
         IloNumVarArray u(env, n);
         for(int i=1; i<n; i++) {
-            u[i] = IloNumVar(env, 1, n-1, ILOINT);
+            name<< "u_"<< i;
+            u[i] = IloNumVar(env, 1, n-1, ILOINT, name.str().c_str());
+            name.str("");
         }
 
+        MTZ[0] = IloRangeArray(env);
         for(int i=1; i<n; i++) {
+            MTZ[i] = IloRangeArray(env, n);
             for(int j=1; j<n; j++) {
                 if(i!=j) {
-                    expr = u[i]-u[j]+n*x[i][j];
-                    model.add(expr <= n-1);
+                    name<< "mtz_"<< i<< "_"<< j;
+                    expr = u[i]-u[j]+(n-1)*x[i][j];
+                    MTZ[i][j] = IloRange(env, -IloInfinity, expr, n-2, name.str().c_str());
+                    name.str("");
                     expr.clear();
                 }
             }
+            model.add(MTZ[i]);
         }
 
         expr.end();
@@ -110,7 +133,7 @@ int main()
 
         IloCplex cplex(model);
 
-        cplex.exportModel("TSP-MTZ_ulysses16.lp");
+        cplex.exportModel("TSP-MTZ_ulysses22.lp");
         cplex.solve();
 
         double sumCost = cplex.getObjValue();
